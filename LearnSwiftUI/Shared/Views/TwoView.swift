d72bbdaa6ce17a24bcd9ebe6d37657b75687a3b0
nick <sinzihlin@gmail.com>
2021-10-25 23:50:04 +0800

    完成siwft ui turtle

---
+++ LearnSwiftUI/Shared/Views/TwoView.swift
@@ -0,0 +1,20 @@
+//
+//  TwoView.swift
+//  LearnSwiftUI
+//
+//  Created by nick on 2021/10/21.
+//
+
+import SwiftUI
+
+struct TwoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TwoView_Previews: PreviewProvider {
+    static var previews: some View {
+        TwoView()
+    }
+}
